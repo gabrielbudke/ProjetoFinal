6766abe456d14da8878bfcf7910a3ce750d7ea00
Logan-Michel-JF <38558603+Logan-Michel-JF@users.noreply.github.com>
2018-08-17 11:22:44 -0300

    inicio e Termino do Obter todos no ProdutoDAO

--- ProjetoFinal/src/java/br/com/projeto/dao/ProdutoDAO.java
+++ ProjetoFinal/src/java/br/com/projeto/dao/ProdutoDAO.java
@@ -5,11 +5,36 @@ import br.com.projeto.database.Conexao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Logan Michel
  */
 public class ProdutoDAO {
+    
+    public List<ProdutoBean> obterTodos(){
+        List<ProdutoBean> produtos = new ArrayList<>();
+        String sql = "SELECT * FROM produtos";
+        try {
+            Statement st = Conexao.obterConexao().createStatement();
+            st.execute(sql);
+            ResultSet resultSet = st.getResultSet();
+            while (resultSet.next()) {
+                ProdutoBean produto = new ProdutoBean();
+                produto.setId(resultSet.getInt("id"));
+                produto.setNome(resultSet.getString("nome"));
+                produto.setPreco(resultSet.getFloat("preco"));
+                produtos.add(produto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            Conexao.fecharConexao();
+        }
+        return produtos;
+    }
   
     public int adicionar (ProdutoBean produto){
         String sql= "INSERT INTO produtos (nome, preco) VALUES" + "(?, ?)";
@@ -30,4 +55,6 @@ public class ProdutoDAO {
         }
         return -1;
     }
+    
+    
 }
