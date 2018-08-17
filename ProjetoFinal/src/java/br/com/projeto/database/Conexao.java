3b9e73defda804de0da0360ae8a0dfe523db18b3
Patrick-Nacimento <38558599+Patrick-Nacimento@users.noreply.github.com>
2018-08-17 08:43:24 -0300

    teste de conexao

--- ProjetoFinal/src/java/br/com/projeto/database/Conexao.java
+++ ProjetoFinal/src/java/br/com/projeto/database/Conexao.java
@@ -1,4 +1,36 @@
 package br.com.projeto.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexao {
-    //TODO criar conexao com o banco de dados
+    private static final String HOST = "jdbc:mysql://localhost/projeto_final";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static final String CLASS = "com.mysql.jdbc.Driver";
+    private static Connection conexao;
+    
+    public static Connection obterConexao(){
+        try{
+            Class.forName(CLASS);
+            conexao = DriverManager.getConnection(HOST, USER, PASSWORD);
+            return conexao;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static void fecharConexao(){
+        try{
+            conexao.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+         Conexao.obterConexao();
+    }
+    
 }
