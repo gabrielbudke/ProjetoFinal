357d6f2598f42bcdbb80111fa64cf28510381233
Gabriel Budke de Sousa <38558555+gabrielbudke@users.noreply.github.com>
2018-08-17 07:55:46 -0300

    Atualização do Repositório

--- ProjetoFinal/src/java/br/com/projeto/database/estrutura.sql
+++ ProjetoFinal/src/java/br/com/projeto/database/estrutura.sql
@@ -3,38 +3,42 @@ CREATE DATABASE projeto_final;
 USE projeto_final;
 
 CREATE TABLE comerciantes(
-	login VARCHAR (30),
-	senha VARCHAR (6),	
-	nome VARCHAR(100),
-	cpf VARCHAR(20),
+	login VARCHAR (30) NOT NULL,
+	senha VARCHAR (6) NOT NULL,	
+	nome VARCHAR(100) NOT NULL,
+	cpf VARCHAR(20) NOT NULL,
 	email VARCHAR(50),
-	telefone VARCHAR (20)
+	telefone VARCHAR(20) NOT NULL,
+        rua VARCHAR(50) NOT NULL,
+        numero VARCHAR(5) NOT NULL,
+        bairro VARCHAR(50) NOT NULL,
+        cidade VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE funcionarios(
 	id INT AUTO_INCREMENT PRIMARY KEY,
-        login VARCHAR(30),
-        senha VARCHAR(6),
-	funcao VARCHAR(50),
-	cpf VARCHAR (20),
-	telefone VARCHAR(20),
-	email VARCHAR(50)
+        login VARCHAR(30) NOT NULL,
+        senha VARCHAR(6) NOT NULL,
+	funcao VARCHAR(50) NOT NULL,
+	cpf VARCHAR (20) NOT NULL,
+	telefone VARCHAR(20) NOT NULL,
+	email VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE endereço_funcionarios(
     id_funcionario INTEGER NOT NULL,
-    rua VARCHAR(50),
+    rua VARCHAR(50) NOT NULL,
     numero VARCHAR(5), 
-    bairro VARCHAR (50)
-    cidade VARCHAR (50)
+    bairro VARCHAR (50) NOT NULL,
+    cidade VARCHAR (50) NOT NULL,
     FOREIGN KEY (id_funcionario) REFERENCES funcionarios(id)
 );
 
 CREATE TABLE fornecedores(
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	nome VARCHAR (100),
-	cnpj VARCHAR (100),
-	telefone VARCHAR (20),
+	nome VARCHAR (100) NOT NULL,
+	cnpj VARCHAR (100) NOT NULL,
+	telefone VARCHAR (20) NOT NULL,
 	email VARCHAR (50)
 );
 
@@ -53,7 +57,7 @@ CREATE TABLE produtos(
 
 CREATE TABLE estoque(
 	id_produto INTEGER NOT NULL ,	
-	tipo VARCHAR(7), -- Entrada ou Saída
+	tipo VARCHAR(7) NOT NULL, -- Entrada ou Saída
 	quantidade INT UNSIGNED,	
 	FOREIGN KEY (id_produto) REFERENCES produtos(id)
 );
