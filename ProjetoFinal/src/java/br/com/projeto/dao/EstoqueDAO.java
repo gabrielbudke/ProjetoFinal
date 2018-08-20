bdf3344c82f74aa4a731acd8d29d94ea03ed636c
Logan-Michel-JF <38558603+Logan-Michel-JF@users.noreply.github.com>
2018-08-20 08:41:00 -0300

    Finalização do ProdutoDAO e inicio do EstoqueDAO

---
+++ ProjetoFinal/src/java/br/com/projeto/dao/EstoqueDAO.java
@@ -0,0 +1,15 @@
+package br.com.projeto.dao;
+
+import br.com.projeto.bean.EstoqueBean;
+
+/**
+ * @author Logan Michel
+ */
+public class EstoqueDAO {
+    
+    public int adicionar(EstoqueBean estoque){
+        String sql = "INSERT INTO estoque id_produto, tipo, quantidade";
+        
+    }
+    
+}
