64018a86489f99d4c210224c593ff1592a8e77a3
Gabriel Budke de Sousa <38558555+gabrielbudke@users.noreply.github.com>
2018-08-21 11:28:08 -0300

    Criação do Saida Produto

--- ProjetoFinal/src/java/br/com/projeto/web/saida/SaidaProduto.java
+++ ProjetoFinal/src/java/br/com/projeto/web/saida/SaidaProduto.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.projeto.web.saida;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
 /**
- *
- * @author Alunos
+ * @author Gabriel Budke
  */
-public class SaidaProduto {
+@WebServlet(urlPatterns = "/produtos/saida")
+public class SaidaProduto extends HttpServlet{
+    
     
 }
