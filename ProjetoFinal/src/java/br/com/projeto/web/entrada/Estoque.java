180e05bb4bf255d5b767c254feb10788a8d19afb
Patrick-Nacimento <38558599+Patrick-Nacimento@users.noreply.github.com>
2018-08-21 09:55:05 -0300

    base do html estoque pronta

--- ProjetoFinal/src/java/br/com/projeto/web/entrada/Estoque.java
+++ ProjetoFinal/src/java/br/com/projeto/web/entrada/Estoque.java
@@ -1,6 +1,7 @@
 package br.com.projeto.web.entrada;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +10,38 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns="/estoque/cadastro")
 public class Estoque extends HttpServlet {
+    
+    private PrintWriter out;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        out = resp.getWriter();
+        out.println("!DOCTYPE html>");
+        out.println("html>");
+        out.println("head>");
+        out.println("title>Estoque Alimentos</title>");
+        out.println("/head>");
+        out.println("body>");
+        out.println("div>");
+        out.println("table>");
+        out.println("thead>");
+        out.println("th>Categoria</th>");
+        out.println("th>Produto</th>");
+        out.println("th>Valor(Un.)</th>");
+        out.println("th>Quantidade</th>");
+        out.println("/thead>");
+        out.println("tbody>");
+        out.println("tr></tr>");
+        out.println("tr></tr>");
+        out.println("tr></tr>");
+        out.println("tr></tr>");
+        out.println("tr></tr>");
+        out.println("/tbody>");
+        out.println("/table>");
+        out.println("/div>");
+        out.println("/body>");
+        out.println("/html>");
+
         
         
         
