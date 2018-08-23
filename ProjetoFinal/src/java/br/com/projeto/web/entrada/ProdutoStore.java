27ed3a0b4f34de0c8de1c31b9c5e153847053868
Logan-Michel-JF <38558603+Logan-Michel-JF@users.noreply.github.com>
2018-08-23 10:51:47 -0300

    Quantidade no ProdutoBean

--- ProjetoFinal/src/java/br/com/projeto/web/entrada/ProdutoStore.java
+++ ProjetoFinal/src/java/br/com/projeto/web/entrada/ProdutoStore.java
@@ -1,5 +1,9 @@
 package br.com.projeto.web.entrada;
 
+import br.com.projeto.bean.CategoriaBean;
+import br.com.projeto.bean.ProdutoBean;
+import br.com.projeto.dao.CategoriaDAO;
+import br.com.projeto.dao.ProdutoDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -14,10 +18,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/produtos/store")
 public class ProdutoStore extends HttpServlet{
     
-    private PrintWriter out;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        ProdutoBean produto = new ProdutoBean();
+        
+        produto.setNome(req.getParameter("nome"));
+        produto.setPreco(Float.parseFloat(req.getParameter("preco")));
+        produto.setQuantidade(Integer.parseInt(req.getParameter("quantidade")));
+       
+        
+        produto.setId(new ProdutoDAO().adicionar(produto));
+        resp.sendRedirect("/produtos/cadastro");
+        
         
     }
     
