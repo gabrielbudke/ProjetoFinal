a6fa02881e9a1e9be2b84391d1db85b97f74d100
Logan-Michel-JF <38558603+Logan-Michel-JF@users.noreply.github.com>
2018-08-23 10:56:09 -0300

    ObterTodos em categoriaDAO

--- ProjetoFinal/src/java/br/com/projeto/dao/CategoriaDAO.java
+++ ProjetoFinal/src/java/br/com/projeto/dao/CategoriaDAO.java
@@ -5,12 +5,38 @@ import br.com.projeto.database.Conexao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Gabriel Budke
  */
 public class CategoriaDAO {
+    
+    public List<CategoriaBean> obterTodos(){
+        List<CategoriaBean> categorias = new ArrayList<>();
+        String sql = "SELECT * FROM categorias";
+        try {
+            Statement st = Conexao.obterConexao().createStatement();
+            st.execute(sql);
+            ResultSet resultSet = st.getResultSet();
+            while(resultSet.next()){
+                CategoriaBean categoria = new CategoriaBean();
+                categoria.setId(resultSet.getInt("id"));
+                categoria.setNome(resultSet.getString("nome"));
+                categorias.add(categoria);
+                
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            Conexao.fecharConexao();
+        }
+        return categorias;
+        
+    }
 
     public int adicionar(CategoriaBean categoria) {
         String sql = "INSERT INTO categorias(nome) VALUES (?)";
