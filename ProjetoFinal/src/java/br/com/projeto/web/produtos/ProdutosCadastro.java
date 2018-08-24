7b3cb22e8f2c11959adf95db284b5cb779fe4104
Gabriel Budke de Sousa <38558555+gabrielbudke@users.noreply.github.com>
2018-08-24 07:36:10 -0300

    Organização dos pacotes

--- ProjetoFinal/src/java/br/com/projeto/web/produtos/ProdutosCadastro.java
+++ ProjetoFinal/src/java/br/com/projeto/web/produtos/ProdutosCadastro.java
@@ -1,4 +1,4 @@
-package br.com.projeto.web.saida;
+package br.com.projeto.web.produtos;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,20 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * @author Gabriel Budke
- */
-@WebServlet(urlPatterns = "/produtos/saida")
-public class ProdutoSaida extends HttpServlet {
+@WebServlet(urlPatterns ="/produtos/cadastro")
+public class ProdutosCadastro extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        req.getRequestDispatcher("/produtos/saida.jsp").include(req, resp);
-
-        
-        
+    
+        req.getRequestDispatcher("/produtos/cadastro.jsp").include(req, resp);
         
     }
-
+    
 }
