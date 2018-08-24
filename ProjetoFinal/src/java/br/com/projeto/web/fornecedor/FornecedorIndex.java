c4367168ce034acb7505837913f9f7c3326f4997
Gabriel Budke de Sousa <38558555+gabrielbudke@users.noreply.github.com>
2018-08-24 11:29:50 -0300

    FuncionarioDAO
    Criação do FuncionarioDAO para adicionar na tabela

--- ProjetoFinal/src/java/br/com/projeto/web/fornecedor/FornecedorIndex.java
+++ ProjetoFinal/src/java/br/com/projeto/web/fornecedor/FornecedorIndex.java
@@ -1,9 +1,24 @@
 package br.com.projeto.web.fornecedor;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author Alunos
  */
-public class FornecedorIndex {
+@WebServlet(urlPatterns="/fornecedores")
+public class FornecedorIndex extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.getRequestDispatcher("fornecedor/index.jsp").include(req, resp);
+    }
+    
     
 }
