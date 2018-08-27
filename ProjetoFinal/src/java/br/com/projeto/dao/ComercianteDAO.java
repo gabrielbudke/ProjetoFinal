47e613164be3019fba850ef67ca058573d6260ff
Patrick-Nacimento <38558599+Patrick-Nacimento@users.noreply.github.com>
2018-08-27 11:34:12 -0300

    arrumar cadastrar do comerciante cadastro

--- ProjetoFinal/src/java/br/com/projeto/dao/ComercianteDAO.java
+++ ProjetoFinal/src/java/br/com/projeto/dao/ComercianteDAO.java
@@ -13,22 +13,25 @@ public class ComercianteDAO {
 
     public int adicionar(ComercianteBean comerciante) {
 
-        String sql = "INSERT INTO comerciantes (login, senha, nome, cpf, email, telefone, rua, bairro, cidade, estado) VALUES"
-                + "(?     ?    ?        ?       ?     ?        ?      ?)";
+        String sql = "INSERT INTO comerciantes (login, senha, nome, cpf, email, telefone, rua, bairro, cidade, estado, numero) VALUES"
+                + "(?,?,?,?,?,?,?,?,?,?,?)";
 
         try {
             PreparedStatement ps = Conexao.obterConexao().prepareStatement(sql,
                     PreparedStatement.RETURN_GENERATED_KEYS);
             int posicao = 1;
-            ps.setString(posicao++, comerciante.getEmail());
+            ps.setString(posicao++, comerciante.getLogin());
             ps.setString(posicao++, comerciante.getSenha());
             ps.setString(posicao++, comerciante.getNome());
             ps.setString(posicao++, comerciante.getCpf());
             ps.setString(posicao++, comerciante.getEmail());
+            ps.setString(posicao++, comerciante.getTelefone());
             ps.setString(posicao++, comerciante.getRua());
             ps.setString(posicao++, comerciante.getBairro());
             ps.setString(posicao++, comerciante.getCidade());
             ps.setString(posicao++, comerciante.getEstado());
+            ps.setString(posicao++, comerciante.getNumero());
+            
             ps.execute();
             ResultSet resultSet = ps.getGeneratedKeys();
             if (resultSet.next()) {
