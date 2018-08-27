bc7a78d6cce0a921ece644b35d828e69db01d2f6
Gabriel Budke de Sousa <38558555+gabrielbudke@users.noreply.github.com>
2018-08-27 08:11:44 -0300

    Atualização da tabela fornecedores

--- ProjetoFinal/src/java/br/com/projeto/web/fornecedor/FornecedoresCadastro.java
+++ ProjetoFinal/src/java/br/com/projeto/web/fornecedor/FornecedoresCadastro.java
@@ -8,17 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns ="/fornecedor/cadastro")
+@WebServlet(urlPatterns = "/fornecedor/cadastro")
 public class FornecedoresCadastro extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    req.getRequestDispatcher("/fornecedor/cadastro.jsp").include(req, resp);
-    
-    
+        req.getRequestDispatcher("/fornecedor/cadastro.jsp").include(req, resp);
+
     }
-    
-            
 
 }
