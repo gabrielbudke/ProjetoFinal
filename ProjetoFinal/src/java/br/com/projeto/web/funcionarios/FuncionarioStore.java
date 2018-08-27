b31bfcc8f89cdeb2a2f1689bbc36efc54ec29885
Patrick-Nacimento <38558599+Patrick-Nacimento@users.noreply.github.com>
2018-08-27 10:28:58 -0300

    iniciando comercianteCdastro e comercianteIndex

--- ProjetoFinal/src/java/br/com/projeto/web/funcionarios/FuncionarioStore.java
+++ ProjetoFinal/src/java/br/com/projeto/web/funcionarios/FuncionarioStore.java
@@ -1,6 +1,8 @@
 package br.com.projeto.web.funcionarios;
 
+import br.com.projeto.bean.EnderecoFuncionarioBean;
 import br.com.projeto.bean.FuncionarioBean;
+import br.com.projeto.dao.EnderecoFuncionarioDAO;
 import br.com.projeto.dao.FuncionarioDAO;
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -20,12 +22,23 @@ public class FuncionarioStore extends HttpServlet{
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
         FuncionarioBean funcionario = new FuncionarioBean();
+        EnderecoFuncionarioBean endereco = new EnderecoFuncionarioBean();
         
         funcionario.setNome(req.getParameter("nome"));
         funcionario.setFuncao(req.getParameter("funcao"));
         funcionario.setCpf(req.getParameter("cpf"));
         funcionario.setTelefone(req.getParameter("telefone"));
+        endereco.setCep(req.getParameter("cep"));
+        endereco.setEstado(req.getParameter("estado"));
+        endereco.setRua(req.getParameter("rua"));
+        endereco.setBairro(req.getParameter("bairro"));
+        endereco.setNumero(req.getParameter("numero"));
+        endereco.setCidade(req.getParameter("cidade"));
         
+        endereco.setIdFuncionario(funcionario.getId());
+
+        
+        endereco.setId(new EnderecoFuncionarioDAO().adicionar(endereco));
         funcionario.setId(new FuncionarioDAO().adicionar(funcionario));
         resp.sendRedirect("/ProjetoFinal/funcionario/cadastro");
         
