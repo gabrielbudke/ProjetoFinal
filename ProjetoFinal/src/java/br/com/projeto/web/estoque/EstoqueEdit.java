c97a78172832c030e6f3490557c179ac10414f72
Alunos <Kaiohschumis@gmail.com>
2018-09-06 10:05:39 -0300

    Alteração banco de dados e tudo mais
    Commit

--- ProjetoFinal/src/java/br/com/projeto/web/estoque/EstoqueEdit.java
+++ ProjetoFinal/src/java/br/com/projeto/web/estoque/EstoqueEdit.java
@@ -1,4 +1,19 @@
 package br.com.projeto.web.estoque;
-public class EstoqueEdit {
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/estoque/editar")
+public class EstoqueEdit extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+    
 
 }
---
+++ ProjetoFinal/web/jsp/funcionario/editar.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : editar
+    Created on : 06/09/2018, 09:10:21
+    Author     : Alunos
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
