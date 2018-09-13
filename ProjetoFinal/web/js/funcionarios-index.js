c8d62d87a14626309afd5e0a2c01075a5a2d782b
Alunos <you@example.com>
2018-09-13 09:52:46 -0300

    Atualização

--- ProjetoFinal/web/js/funcionarios-index.js
+++ ProjetoFinal/web/js/funcionarios-index.js
@@ -1,12 +1,18 @@
-$(function(){
-   $("#funcionario-index").DataTable({
-       "ajax": "/funcionarios/obtertodosparadatatable",
-       "columns": [
-           {"data": "id"},
-           {"data": "nome"},
-           {"data": "funcao"},
-           {"data": "telefone"},
-           {"data": "telefone"}
-       ]
-   }); 
+$(function () {
+    $("#funcionario-index").DataTable({
+        "ajax": "/funcionarios/obtertodosparadatatable",
+        "columns": [
+            {"data": "id"},
+            {"data": "nome"},
+            {"data": "funcao"},
+            {"data": "telefone"},
+            {
+                "data": null,
+                "render": function (data) {
+                    return "<a class='btn btn-info'  href='/produtos/editar?id=" + data.id + "'><i class='fas fa-edit'></i> Editar</a>\
+                    <a class='btn btn-danger' href='/produtos/excluir?id=" + data.id + "'><i class='fas fa-trash-alt'></i> Excluir</a>";
+                }
+            }
+        ]
+    });
 });
