e4861dee807ba8446f92239508833301e9632944
Alunos <you@example.com>
2018-09-14 11:27:42 -0300

    Validação no campo nome em produtos cadastro

--- ProjetoFinal/src/java/br/com/projeto/web/produtos/ProdutoStore.java
+++ ProjetoFinal/src/java/br/com/projeto/web/produtos/ProdutoStore.java
@@ -17,40 +17,32 @@ import javax.servlet.http.HttpServletResponse;
  * @author Logan Michel
  */
 @WebServlet(urlPatterns = "/produtos/store")
-public class ProdutoStore extends HttpServlet{
-    
+public class ProdutoStore extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProdutoBean produto = new ProdutoBean();
-	//Inserindo dados no ProdutoBean
+        //Inserindo dados no ProdutoBean
         produto.setNome(req.getParameter("nome"));
-       
-        try{
-                    
+        //produto.setPreco(Float.parseFloat(req.getParameter("preco")));
+
+        try {
+
             float preco = Float.parseFloat(req.getParameter("preco"));
             produto.setPreco(preco);
-        }catch(NumberFormatException e){
-            System.out.print("Vcilao");
+        } catch (NumberFormatException e) {
+            //resp.getWriter().print("alert(Somente Numero)");
+            resp.sendRedirect("/produtos");
         }
-       
-        
-       
-        
-	produto.setIdCategoria(Integer.parseInt(req.getParameter("categoria")));
+
+        produto.setIdCategoria(Integer.parseInt(req.getParameter("categoria")));
         produto.setId(new ProdutoDAO().adicionar(produto));
-        
-        /*EstoqueBean estoque = new EstoqueBean();
+
+        /*
+        EstoqueBean estoque = new EstoqueBean();
         estoque.setIdProduto(Integer.parseInt(req.getParameter("")));
-        estoque.setId(new EstoqueDAO().adicionar(estoque));*/
-        
-        
-        
-        
-        resp.sendRedirect("/produtos");
-        
-     
+        estoque.setId(new EstoqueDAO().adicionar(estoque));
+         */
     }
-    
-    
+
 }
