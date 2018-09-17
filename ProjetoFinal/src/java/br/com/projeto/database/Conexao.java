774eab01e0d61015d9efb918dfc9cb5c9197392a
Alunos <Kaiohschumis@gmail.com>
2018-09-17 10:02:46 -0300

    Alteração banco

--- ProjetoFinal/src/java/br/com/projeto/database/Conexao.java
+++ ProjetoFinal/src/java/br/com/projeto/database/Conexao.java
@@ -4,33 +4,40 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Conexao {
+
     private static final String HOST = "jdbc:mysql://localhost/projeto_final";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static String USER = "root";
+    private static String PASSWORD = "";
     private static final String CLASS = "com.mysql.jdbc.Driver";
     private static Connection conexao;
-    
-    public static Connection obterConexao(){
-        try{
+
+    public static Connection obterConexao() {
+        try {
             Class.forName(CLASS);
-            conexao = DriverManager.getConnection(HOST, USER, PASSWORD);
+
+            /*String dbName = System.getProperty("RDS_DB_NAME");
+            USER = System.getProperty("RDS_USERNAME");
+            PASSWORD = System.getProperty("RDS_PASSWORD");
+            String hostname = System.getProperty("RDS_HOSTNAME");
+            String port = System.getProperty("RDS_PORT");
+            conexao = DriverManager.getConnection(HOST, USER, PASSWORD);*/
             return conexao;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-    
-    public static void fecharConexao(){
-        try{
+
+    public static void fecharConexao() {
+        try {
             conexao.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-   /* public static void main(String[] args) {
+
+    /* public static void main(String[] args) {
          Conexao.obterConexao();
     }
-    */
+     */
 }
--- ProjetoFinal/src/java/br/com/projeto/database/estrutura.sql
+++ ProjetoFinal/src/java/br/com/projeto/database/estrutura.sql
@@ -107,6 +107,10 @@ CREATE TABLE recuperar_senha(
 );
 
 */
-INSERT INTO funcionarios (nome, funcao, telefone) VALUES
-('Patrick', 'Estudante', '(83) 3542-2574'
-);
+INSERT INTO funcionarios (nome,login, senha, telefone) VALUES
+('Patrick', 'patrick', '123', '(83) 3542-2574'),
+('Patrick Lejinho', 'patrick_lejinho', '123', '(43) 2847-4595'),
+('Lucas Tomando', 'lucas_tomando', '123', '(43) 98680-5360'),
+('João Paulo', 'joao_paulo', '123', '(84) 3507-3289'),
+('Isadora das Costas Largas', 'isadora', '123', '(84) 98104-9974'),
+('Jorge Tijolinho', 'jorge_tijolinho', '123', '(84) 98104-9974');
